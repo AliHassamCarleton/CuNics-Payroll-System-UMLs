15df83cc5813da6d2f62538ee96de5a682e1afa7
AliHassamCarleton <alihassam@cmail.carleton.ca>
2017-03-05 13:50:26 -0500

    Add files via upload

---
+++ Employee.cc
@@ -0,0 +1,13 @@
+
+using namespace std;
+#include "Employee.h"
+
+Employee::Employee(string employeeName){
+
+	name= name;
+
+
+
+}
+
+
---
+++ Employee.h
@@ -0,0 +1,24 @@
+
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include "defs.h"
+
+
+
+class Employee{
+
+	public:
+
+
+
+	protected:
+		string name;
+		int IDNumber;
+		
+
+
+
+
+
+}
